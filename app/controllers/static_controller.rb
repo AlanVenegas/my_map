8b41165ca9a8fdfa149f6a4964a29e393e84ba41
Alan Venegas <avenegal@gmail.com>
2017-05-10 18:55:43 +0200

    Create list and delete addresses

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,13 @@
 class StaticController < ApplicationController
   def map
+  	@addresses = Address.all
   end
 
   def save_address
     Address.create(latitude: params[:latitude], longitude: params[:longitude], address: params[:address])
+  end
+
+  def delete_addresses
+  	Address.delete_all
   end
 end
