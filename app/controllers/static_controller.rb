6877797d46ec7a5a66767b53b2ddeebc2ef53a4b
Alan Venegas <avenegal@gmail.com>
2017-05-10 14:25:25 +0200

    Save address in DB

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
 class StaticController < ApplicationController
   def map
   end
+
+  def save_address
+    Address.create(latitude: params[:latitude], longitude: params[:longitude], address: params[:address])
+  end
 end
